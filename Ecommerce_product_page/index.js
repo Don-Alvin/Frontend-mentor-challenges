d8dd9eaf92bd58f00c28db967ad06438beaa2b37
Don-Alvin <alvindon41@gmail.com>
2023-01-12 17:10:42 +0300

    Mobile menu done

--- Ecommerce_product_page/index.js
+++ Ecommerce_product_page/index.js
@@ -1,6 +1,13 @@
+// Sliding Menu
 const hamburgerElement = document.querySelector(".hamburger-group-icon");
 const menu = document.querySelector(".mobile_menu");
 const closeElement = document.querySelector(".close_icon");
+
+// Images
+const mainImage = document.querySelector(".main-image");
+const otherImages = document.querySelectorAll(".other-image");
+
+// Cart
 
 const openMenu = () => {
 	menu.classList.add("show_menu");
@@ -9,6 +16,15 @@ const openMenu = () => {
 const closeMenu = () => {
 	menu.classList.remove("show_menu");
 };
+
+let otherImage;
+for (let i = 0; i < otherImages.length; i++) {
+	otherImage = otherImages[i];
+	const replaceImage = () => {
+		mainImage.src = otherImages[i].src;
+	};
+	otherImage.addEventListener("click", replaceImage);
+}
 
 hamburgerElement.addEventListener("click", openMenu);
 closeElement.addEventListener("click", closeMenu);
