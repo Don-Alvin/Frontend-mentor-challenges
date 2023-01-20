f6bc3d8977e929c6e06ff461a3ca18f277356220
Don-Alvin <alvindon41@gmail.com>
2023-01-21 02:22:17 +0300

    Scroll behavior smooth added

--- news_homepage/index.js
+++ news_homepage/index.js
@@ -19,7 +19,12 @@ const showScrollIcon = () => {
 	}
 };
 
+// const scrollUp = () => {
+// 	this.scrollY = 0;
+// };
+
 openMenuElement.addEventListener("click", openMenu);
 closeMenuElement.addEventListener("click", closeMenu);
 slidingMenu.addEventListener("click", closeMenu);
 window.addEventListener("scroll", showScrollIcon);
+// scrollUpIcon.addEventListener("click", scrollUp);
